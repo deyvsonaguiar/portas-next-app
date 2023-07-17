94ae321398ceea6ed220c46995b65aca27ec5776
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2023-07-17 12:31:54 -0400

    Criando função criarPortas com Array.from

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,12 @@ import { useState } from 'react';
 import Porta from '../components/Porta';
 
 import PortaModel from '../model/porta';
+import { criarPortas } from '../functions/portas';
 
 export default function Home() {
   const [p1, setP1] = useState(new PortaModel(1));
+
+  console.log(criarPortas(3, 2))
 
   return (
     <div style={{ display: 'flex' }}>
